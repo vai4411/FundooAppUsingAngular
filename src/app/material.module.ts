683d84742806f1220fe0c8d5b755aea9633846f0
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-09-23 21:02:23 +0530

    [Vaibhav]ADD: angular material external file for access material

---
+++ src/app/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  imports: [],
+  exports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+  ],
+})
+export class MaterialModule {}
